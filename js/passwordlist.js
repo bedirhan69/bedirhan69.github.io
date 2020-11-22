2f367b8951b298537867587154058b9b817bb1d8
bedirhan69 <72661307+bedirhan69@users.noreply.github.com>
2020-11-23 00:02:27 +0300

    Create passwordlist.js

---
+++ js/passwordlist.js
@@ -0,0 +1,10 @@
+var isim;
+var görüş;
+var ad="123";
+var pass1="123";
+isim=prompt('Sayfayi görebilmek icin lütfen Kullanıcı adını giriniz!','');
+görüş=prompt('Sayfayi görebilmek icin lütfen şifreyi giriniz!','');
+if(isim==ad){alert('Anlayışınız için teşekkürler');}
+if (görüş==pass1){;}
+else{window.location="502.html";}
+
