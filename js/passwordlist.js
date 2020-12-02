bcb732c715dce740d43afbca404edf2c21c47bbb
bedirhan69 <72661307+bedirhan69@users.noreply.github.com>
2020-12-02 20:56:41 +0300

    Update passwordlist.js

--- js/passwordlist.js
+++ js/passwordlist.js
@@ -1,7 +1,7 @@
 var isim;
 var görüş;
-var ad="123";
-var pass1="123";
+var ad="12345";
+var pass1="12345";
 isim=prompt('Sayfayi görebilmek icin lütfen Kullanıcı adını giriniz!','');
 görüş=prompt('Sayfayi görebilmek icin lütfen şifreyi giriniz!','');
 if(isim==ad){alert('Anlayışınız için teşekkürler');}
