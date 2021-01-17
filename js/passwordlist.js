837f3526d80d1c710421f6f4418ee3632b493adb
bedirhan69 <72661307+bedirhan69@users.noreply.github.com>
2021-01-17 18:19:48 +0300

    Update passwordlist.js

--- js/passwordlist.js
+++ js/passwordlist.js
@@ -1,10 +1,8 @@
 var isim;
 var görüş;
-var ad="12345";
-var pass1="12345";
-isim=prompt('Sayfayi görebilmek icin lütfen Kullanıcı adını giriniz!','');
-görüş=prompt('Sayfayi görebilmek icin lütfen şifreyi giriniz!','');
-if(isim==ad){alert('Anlayışınız için teşekkürler');}
-if (görüş==pass1){;}
-else{window.location="502.html";}
-
+var ad = "ozi";
+var pass1 = "ozi";
+isim = prompt('Sayfayi görebilmek icin lütfen Kullanıcı adını giriniz!', '');
+görüş = prompt('Sayfayi görebilmek icin lütfen şifreyi giriniz!', '');
+if (isim == ad) { alert('Anlayışınız için teşekkürler'); }
+if (görüş == pass1) {; } else { window.location = "502.html"; }
