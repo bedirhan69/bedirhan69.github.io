72bc021143acb7b4008358ded3c70d5a32041a98
bedirhan69 <72661307+bedirhan69@users.noreply.github.com>
2021-01-22 20:59:29 +0300

    Update hesaplama.js

--- js/hesaplama.js
+++ js/hesaplama.js
@@ -15,11 +15,7 @@ function hesapla() {
 
     not1 = (s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9 + s10 + s11) / 30.0;
 
-    if (s2 > 00 && s2 < 55)
-        harfnot = 'türkçe 55 altı';
-    else if (s1 > 00 && s1 < 45)
-        harfnot = 'Zayıf Ders var';
-    else if (not1 >= 85 && not1 <= 100)
+    if (not1 >= 85 && not1 <= 100)
         harfnot = 'Tebrikler! Taktir belgesini Hak kazandınız!';
     else if (not1 >= 70 && not1 <= 85)
         harfnot = 'Tebrikler! Teşekkür belgesini Hak kazandınız!';
@@ -27,6 +23,10 @@ function hesapla() {
         harfnot = 'Malesef hiç bir belge kazanamadınız!';
 
 
+    if (s1 > 00 && s1 < 45)
+        harfnot = 'Zayıf Ders var';
+    if (s2 > 00 && s2 < 55)
+        harfnot = 'Türkçe 55 altı';
     if (s3 > 00 && s3 < 45)
         harfnot = 'Zayıf Ders var';
     if (s4 > 00 && s4 < 45)
@@ -46,7 +46,9 @@ function hesapla() {
     if (s11 > 00 && s11 < 45)
         harfnot = 'Zayıf Ders var';
     if (not1 > 100)
-        alert('Lütfen Dersleri 100 üzerinden gir!!!');
+        alert('Lütfen Dersleri 100 üzerinden gir!!!')
+        return=false;
+      else return=true;
     if (s2 > 00 && s2 < 55)
         harfnot = 'türkçe 55 altı';
 
