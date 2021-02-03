6856bdbd22e3afa4f0b6c10e8694ea8a02027a46
bedirhan69 <72661307+bedirhan69@users.noreply.github.com>
2021-02-03 19:31:06 +0300

    Update passwordlist.js

--- js/passwordlist.js
+++ js/passwordlist.js
@@ -4,5 +4,6 @@ var ad = "matematik";
 var pass1 = "matematik";
 isim = prompt('Sayfayi görebilmek icin lütfen Kullanıcı adını giriniz!', '');
 görüş = prompt('Sayfayi görebilmek icin lütfen şifreyi giriniz!', '');
-if (isim == ad) { alert('Anlayışınız için teşekkürler'); }
-if (görüş == pass1) {; } else { window.location = "502.html"; }
+if (isim == ad && görüş == pass1) {
+    alert('Anlayışınız için teşekkürler');
+} else { window.location = "404.html"; }
