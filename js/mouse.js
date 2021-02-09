f1d64658a725a0e66bece5ff3ed99d6bc451d818
bedirhan69 <72661307+bedirhan69@users.noreply.github.com>
2021-02-09 19:30:43 +0300

    Update mouse.js

--- js/mouse.js
+++ js/mouse.js
@@ -1,13 +1,16 @@
-document.oncontextmenu = function(){return false}
-if(document.layers) 
-{
-	window.captureEvents(Event.MOUSEDOWN);
-	window.onrightmousedown = function(e)
-	{
-	if(e.target==document)return false;
-	}
+document.oncontextmenu = function() { return false }
+if (document.layers) {
+    window.captureEvents(Event.MOUSEDOWN);
+    window.onrightmousedown = function(e) {
+        if (e.target == document) return false;
+    }
+} else {
+    document.onrightmousedown = function() { return false }
 }
-else 
-{
-	document.onrightmousedown = function(){return false}
+var kutu = document.getElementById("123");
+window.onkeydown = function(olay) {
+    if (olay.keyCode == 85) {
+        alert('Dostum Sayfanın yazılımı bizde kalsın')
+        return false
+    }
 }
