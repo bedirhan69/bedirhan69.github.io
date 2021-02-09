c62b5e21c8ab78abe62147ca126b4149d89ec523
bedirhan69 <72661307+bedirhan69@users.noreply.github.com>
2021-02-09 22:58:50 +0300

    Update mouse.js

--- js/mouse.js
+++ js/mouse.js
@@ -3,9 +3,10 @@ if (document.layers) {
     window.captureEvents(Event.MOUSEDOWN);
     window.onrightmousedown = function(e) {
         if (e.target == document) return false;
+
     }
 } else {
-    document.onrightmousedown = function() { return false }
+    document.onrightmousedown = function() { return false; }
 }
 var kutu = document.getElementById("123");
 window.onkeydown = function(olay) {
