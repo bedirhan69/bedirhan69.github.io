36a2897bf0afa699abacbc660addbfe32ef834cf
bedirhan69 <72661307+bedirhan69@users.noreply.github.com>
2021-02-12 15:20:10 +0300

    Update mouse.js

--- js/mouse.js
+++ js/mouse.js
@@ -15,3 +15,10 @@ window.onkeydown = function(olay) {
         return false
     }
 }
+var kutu = document.getElementById("123");
+window.onkeydown = function(olay) {
+    if (olay.keyCode == 123) {
+        alert('Dostum Sayfanın yazılımı bizde kalsın')
+        return false
+    }
+}
