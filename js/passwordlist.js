b8c589821b315361b8b2aec14e70f6bb79391dfb
bedirhan69 <72661307+bedirhan69@users.noreply.github.com>
2021-02-16 11:37:01 +0300

    Update passwordlist.js

--- js/passwordlist.js
+++ js/passwordlist.js
@@ -1,7 +1,7 @@
 var isim;
 var görüş;
-var ad = "matematik";
-var pass1 = "matematik";
+var ad = "123";
+var pass1 = "123";
 isim = prompt('Sayfayi görebilmek icin lütfen Kullanıcı adını giriniz!', '');
 görüş = prompt('Sayfayi görebilmek icin lütfen şifreyi giriniz!', '');
 if (isim == ad && görüş == pass1) {
