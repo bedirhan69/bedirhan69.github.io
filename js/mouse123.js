62763fbb70abbdbe970784cc65348148d071ad4b
bedirhan69 <72661307+bedirhan69@users.noreply.github.com>
2021-02-16 11:36:53 +0300

    Create mouse123.js

---
+++ js/mouse123.js
@@ -0,0 +1,7 @@
+var kutu = document.getElementById("123");
+window.onkeydown = function(olay) {
+    if (olay.keyCode == 123) {
+        alert('Dostum Sayfanın yazılımı bizde kalsın')
+        return false
+    }
+}
