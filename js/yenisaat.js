0ec19ca5c65558185beb868213ff8326c926708c
bedirhan69 <72661307+bedirhan69@users.noreply.github.com>
2021-02-20 00:00:46 +0300

    Update yenisaat.js

--- js/yenisaat.js
+++ js/yenisaat.js
@@ -1,39 +1,38 @@
-$(document).ready(function() {
-    $('.countdown').final_countdown({
-        start: '0000000000',
-        //000000.00dk.00sn
-        //000010000== 2saat 50dk
-        //000100000 == 1gün 3saat
-        //01000000 == 11gün 13saat 46dk
-        end: '00001320000',
-        now: '0000000000',
-        selectors: {
-            value_seconds: '.clock-seconds .val',
-            canvas_seconds: 'canvas_seconds',
-            value_minutes: '.clock-minutes .val',
-            canvas_minutes: 'canvas_minutes',
-            value_hours: '.clock-hours .val',
-            canvas_hours: 'canvas_hours',
-            value_days: '.clock-days .val',
-            canvas_days: 'canvas_days'
-        },
-        seconds: {
-            borderColor: '#7995D5',
-            borderWidth: '5'
-        },
-        minutes: {
-            borderColor: '#ACC742',
-            borderWidth: '5'
-        },
-        hours: {
-            borderColor: '#ECEFCB',
-            borderWidth: '5'
-        },
-        days: {
-            borderColor: '#FF9900',
-            borderWidth: '5'
-        }
-    }, function() {
-        // Finish callback
-    });
-});
+const year = new Date().getFullYear();
+const fourthOfJuly = new Date(year, 2, 0).getTime();
+const fourthOfJulyNextYear = new Date(year + 1, 6, 4).getTime();
+const month = new Date().getMonth();
+
+// countdown
+let timer = setInterval(function() {
+
+    // get today's date
+    const today = new Date().getTime();
+
+    // get the difference
+    let diff;
+    if (month > 6) {
+        diff = fourthOfJulyNextYear - today;
+    } else {
+        diff = fourthOfJuly - today;
+    }
+
+    // math
+    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    // display
+    document.getElementById("timer").innerHTML =
+        "<div class=\"days\"> \
+<div class=\"numbers\">" + days + "</div>Gün</div> \
+<div class=\"hours\"> \
+<div class=\"numbers\">" + hours + "</div>Saat</div> \
+<div class=\"minutes\"> \
+<div class=\"numbers\">" + minutes + "</div>Dakika</div> \
+<div class=\"seconds\"> \
+<div class=\"numbers\">" + seconds + "</div>Saniye</div> \
+</div>";
+
+}, 1000);
