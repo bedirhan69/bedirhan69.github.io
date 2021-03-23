bb5b8ab495058816989c7b488d91e821c7c2f25d
bedirhan69 <72661307+bedirhan69@users.noreply.github.com>
2021-03-23 12:08:19 +0300

    Update yenisaat.js

--- js/yenisaat.js
+++ js/yenisaat.js
@@ -1,6 +1,6 @@
 const year = new Date().getFullYear();
-const fourthOfJuly = new Date(year, 2, 18).getTime(); //18-2=16
-const fourthOfJulyNextYear = new Date(year + 1, 6, 4).getTime();
+const fourthOfJuly = new Date(year, 2, 30).getTime(); //18-2=16
+const fourthOfJulyNextYear = new Date(year + 2, 6, 4).getTime();
 const month = new Date().getMonth();
 
 // countdown
