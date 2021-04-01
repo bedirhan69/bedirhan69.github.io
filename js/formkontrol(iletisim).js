4924b6aa37c49dfac282fdda2ccbb86ee0cc0ac6
bedirhan69 <72661307+bedirhan69@users.noreply.github.com>
2021-04-01 10:32:12 +0300

    Update formkontrol(iletisim).js

--- js/formkontrol(iletisim).js
+++ js/formkontrol(iletisim).js
@@ -1,19 +1,18 @@
 "use strict";
-var formGiris = document.forms["forum2"];  // form seçimi
-var eposta = formGiris.eposta;   // eposta alanı seçimi
-var sifre = formGiris["isim"];  // sifre alanı seçimi
-var text = formGiris["görüş"];  //e-posta veya telefon
+var formGiris = document.forms["forum2"]; // form seçimi
+var eposta = formGiris.eposta; // eposta alanı seçimi
+var sifre = formGiris["isim"]; // sifre alanı seçimi
+var text = formGiris["görüş"]; //e-posta veya telefon
 
 formGiris.onsubmit = function() {
-  if (sifre.value.length > 2) {
-    
-  } else {
-    alert("Lütfen adınızı doğru giriniz!");
-    return false;  // form gönderimini iptal et
-  }
-if (text.value.length > 12) {
-}else{
-  alert("Görüşünüzü giriniz!")
-  return false; //form gönderimini iptal et
-}
+    if (sifre.value.length > 2) {
+
+    } else {
+        alert("Lütfen adınızı doğru giriniz!");
+        return false; // form gönderimini iptal et
+    }
+    if (text.value.length > 12) {} else {
+        alert("Görüşünüzü giriniz!")
+        return false; //form gönderimini iptal et
+    }
 }
